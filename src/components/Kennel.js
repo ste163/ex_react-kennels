8bd9b1c012f8a1635e4e86db2e2e59db46e219f7
ste163 <samtedwards@pm.me>
2020-10-02 11:50:37 -0400

    added animal card

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,6 +1,7 @@
 // the react from looks different because it's looking in the package.json file
-import React from "react" 
-import "./Kennel.css"
+import React from "react"
+import { AnimalCard } from "./animal/AnimalCard"
+import "./animal/Animal.css"
 
 // In a function, you can only return one thing. That's what the <> </> are. They are "fragments" which say the implicit return should 
 // return all these individual html strings as one chunk.
@@ -8,9 +9,17 @@ export const Kennel = () => (
     <>
         <h2>Nashville Kennels</h2>
         <small>Loving care when you're not there.</small>
+
         <address>
             <div>Visit Us at the Nashville North Location</div>
             <div>500 Puppy Way</div>
         </address>
+
+        <h2>Animals</h2>
+        <article className="animals">
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+        </article>
     </>
 )
